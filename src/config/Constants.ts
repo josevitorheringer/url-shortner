4d320af59546a42683eabe893f2dfe82b1befd03
José Vítor Heringer <46639621+josevitorheringer@users.noreply.github.com>
2021-11-24 06:30:50 -0300

    finish

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION:
+    "mongodb+srv://root:root@urlshortner.7fnqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
